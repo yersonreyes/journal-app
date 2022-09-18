ac271c2963753493d733e7eeab7e799675b642fd
Yerson Reyes <yerson.programador@gmail.com>
2022-09-18 02:00:03 -0300

    add formValid

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,44 @@
-import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 import { AuthLayout } from "../layout/AuthLayout";
 
+const formData = {
+  email: "yerson.programador@gmail.com",
+  password: "123456",
+  displayName: "yerson reyes",
+};
+
+const formValidations = {
+  email: [(value) => value.includes("@"), "El correo debe tener un @"],
+  password: [
+    (value) => value.length >= 6,
+    "El password debe tener mas de 6 caracteres",
+  ],
+  displayName: [(value) => value.length >= 1, "El nombre es obligatorio"],
+};
 export const RegisterPage = () => {
+  const {
+    onInputChange,
+    formState,
+    email,
+    password,
+    displayName,
+    formValid,
+    emailValid,
+    passwordValid,
+    displayValid,
+  } = useForm(formData, formValidations);
+
+  console.log(emailValid);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, password, displayName });
+  };
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +46,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,6 +57,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@correo.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -31,12 +69,15 @@ export const RegisterPage = () => {
               type="password"
               placeholder="contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear una cuenta
               </Button>
             </Grid>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,9 +20,23 @@ export const useForm = (initialForm = {}) => {
     setFormState(initialForm);
   };
 
+  const createValidators = () => {
+    const formCheckedValues = {};
+    for (const formField of Object.keys(formValidations)) {
+      const [fn, errorMesage] = formValidations[formField];
+
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMesage;
+    }
+
+    setFormValidation(formCheckedValues);
+  };
+
   return {
     ...formState,
     onInputChange,
     onReset,
+    ...formValidation,
   };
 };
