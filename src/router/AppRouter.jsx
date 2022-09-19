28b0379d4160543dfd45316fe0b278cce0b0f8bf
Yerson Reyes <yerson.programador@gmail.com>
2022-09-19 00:03:56 -0300

    add checking auth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { useCheckAuth } from "../hooks/useCheckAuth";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
 
 export const AppRouter = () => {
+  const { status } = useCheckAuth();
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <Routes>
-      {/* login y registro */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      {/* JournalApp */}
-      <Route path="/*" element={<JournalRoutes />} />
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
